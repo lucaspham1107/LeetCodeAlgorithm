ad36cf221b89261d8973672c07f2c9759c677563
LucasWeinstein <truongphamph1107@gmail.com>
2023-01-29 21:23:49 +0700

    -circle linked list and detect circle solution

--- src/main/kotlin/LinkedListSolution.kt
+++ src/main/kotlin/LinkedListSolution.kt
@@ -18,7 +18,7 @@ object LinkedListSolution {
      */
 
     fun reverseList(head: ListNode?): ListNode? {
-        if(head == null) return null
+        if (head == null) return null
         var dummy = head
         var temp = head
         val arr = mutableListOf<Int>()
@@ -31,5 +31,53 @@ object LinkedListSolution {
             temp = temp?.next
         }
         return head
+    }
+
+    /**
+     * null -> 1->2->3->4->5 -> null -> null <- 1 <- 2
+     *
+     */
+    fun reverseListV2(head: ListNode?): ListNode? {
+        var prev: ListNode? = null
+        var current = head
+        while (current != null) {
+            val next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        }
+        return prev
+    }
+
+    fun detectCycle(head: ListNode?): ListNode? {
+        var slow = head
+        var fast = head
+        var entry = head
+        while (fast?.next != null) {
+            slow = slow?.next
+            fast = fast.next?.next
+            if (slow === fast) {
+                while (slow !== entry) {
+                    slow = slow?.next
+                    entry = entry?.next
+                }
+                return entry
+            }
+        }
+        return null
+    }
+
+    fun hasCycle(head: ListNode?): Boolean {
+        var fast = head
+        var slow = fast
+        while (fast?.next != null) {
+            fast = fast.next?.next
+            slow = slow?.next
+            if (slow != null) {
+                if(fast == slow)
+                    return true
+            }
+        }
+        return false
     }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,5 +2,7 @@ fun main(args: Array<String>) {
     val nodes = ListNode(0)
     nodes.next = ListNode(1)
     nodes.next?.next = ListNode(2)
-    println( LinkedListSolution.reverseList(nodes).toString())
+    nodes.next?.next?.next = ListNode(1)
+    println(nodes.toString())
+    println(LinkedListSolution.hasCycle(nodes))
 }
